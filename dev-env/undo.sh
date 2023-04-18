0d02e2aa16fbc5e616a4292b2d41cb597a126893
yelarjouni <younes.elarjouni@imbe.fr>
2023-04-18 15:50:06 +0200

    Update the undo script

--- dev-env/undo.sh
+++ dev-env/undo.sh
@@ -1,4 +1,22 @@
+echo "Delete the code-server deployment, service, route, and persistent volume claim (pvc) and persistent volume (pv) in the $(oc project -q) project"
 oc delete -f deploy.yaml -f svc.yaml -f route.yaml -f pvc.yaml -n test
 oc delete -f pv.yaml
 
-oc logout
+echo "Delete the unnecessary files"
+rm -f deploy.yaml svc.yaml route.yaml pv.yaml pvc.yaml
+
+echo "Do you want to delete the project? (y/n)"
+read delete_project
+
+if [ $delete_project == "y" ]; then
+    echo "Delete the project"
+    oc delete project $project
+fi
+
+echo "Do you want to logout from the cluster? (y/n)"
+read logout
+
+if [ $logout == "y" ]; then
+    echo "Logout from the cluster"
+    oc logout
+fi
